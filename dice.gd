8d251568ed0663035696a11002c6b9d2c6d18c29
xander6424 <xanderigross@gmail.com>
2026-06-03 15:24:19 -0400

    Dice Roll Code Tweaks
    Altered the code to prevent random duplicate faces and other changes.

--- dice.gd
+++ dice.gd
@@ -7,24 +7,30 @@ signal roll_done(index: int)
 
 var rolling = false
 var current_index = 0
+var new_index = 0
 
 # Runs when node(s) enter the scene tree
 func _ready() -> void:
 	for face in faces.get_children():
 		face.hide()
 	
-	faces.get_child(0).show()
-	
+	# Select a starting face at random
+	var starting_index = faces.get_children().pick_random().get_index()
+	faces.get_child(starting_index).show()
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Roll") and !rolling:
-		roll_dice()
-		
-func roll_dice():
-	var duration = 1.0
+		_roll_dice()
+
+func _roll_dice():
+	var duration = 0.8
 	rolling = true
 	
 	while duration > 0:
-		var new_index = faces.get_children().pick_random().get_index()
+		# Prevent duplicate faces from being shown
+		while current_index == new_index:
+			new_index = faces.get_children().pick_random().get_index()
+		
 		faces.get_child(current_index).hide()
 		faces.get_child(new_index).show()
 		
@@ -32,7 +38,7 @@ func roll_dice():
 		
 		current_index = new_index
 		duration -= 0.1
-		
+	
 	rolling = false
 	
 	roll_done.emit(current_index + 1)
