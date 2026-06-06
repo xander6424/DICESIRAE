307e92d78824150676968f4f729da7fb4fb50417
xander6424 <xanderigross@gmail.com>
2026-06-06 15:40:06 -0400

    Fixed Labels
    Slightly fixed label updating issue.

--- main.gd
+++ main.gd
@@ -4,6 +4,9 @@ extends Node2D
 @onready var lots_label: Label = %Lots
 @onready var reroll_label: Label = %Rerolls
 
+# Use this???
+@onready var dice_set: Node2D = $"Dice Set"
+
 var dice_in_play = 5
 var roll_total = 0
 var dice_rolled = 0
@@ -13,15 +16,19 @@ var playing = true
 
 func _ready() -> void:
 	update_score(roll_total)
+	update_labels()
 
 func _process(delta: float) -> void:
-	if rerolls <= 0:
-		lots -= 1
-		rerolls = 2
-		update_score(roll_total)
-		if lots <= 0:
-			pass
-		pass
+	if playing:
+		if rerolls <= 0:
+			lots -= 1
+			rerolls = 2
+			update_labels()
+			 
+			if lots <= 0:
+				# No more rolling, then
+				playing = false
+				enter_shop()
 
 func _on_dice_roll_done(roll: int) -> void:
 	roll_total += roll
@@ -31,6 +38,7 @@ func _on_dice_roll_done(roll: int) -> void:
 	if dice_rolled == dice_in_play:
 		rerolls -= 1
 		update_score(roll_total)
+		update_labels()
 		
 		# Reset for new roll
 		dice_rolled = 0
@@ -38,9 +46,10 @@ func _on_dice_roll_done(roll: int) -> void:
 
 func update_score(score_total: int):
 	score_label.text = str(score_total)
+	
+func update_labels():
 	lots_label.text = "Lots: " + str(lots)
 	reroll_label.text = "Rerolls: " + str(rerolls)
 	
-# func enter_shop():
-	# Buy random game piece
-	# Return to game with reverted lot and reroll count
+func enter_shop():
+	print("SHOP")
