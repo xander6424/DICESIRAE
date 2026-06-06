e6148c6aa7a2c4a86811cc0a226654bb95f3e2bc
xander6424 <xanderigross@gmail.com>
2026-06-06 10:22:31 -0400

    Game Logic & Tweaks
    Added hands and discards, along with them counting down correctly. Soon preparing for further game logic tweaks.

--- main.gd
+++ main.gd
@@ -1,22 +1,35 @@
 extends Node2D
 
 @onready var score_label: Label = %Score
-@onready var rolls_label: Label = %Rolls
+@onready var lots_label: Label = %Lots
+@onready var reroll_label: Label = %Rerolls
 
 var dice_in_play = 5
 var roll_total = 0
 var dice_rolled = 0
-var rolls = 3
+var lots = 3
+var rerolls = 2
+var playing = true
 
 func _ready() -> void:
 	update_score(roll_total)
+
+func _process(delta: float) -> void:
+	if rerolls <= 0:
+		lots -= 1
+		rerolls = 2
+		update_score(roll_total)
+		if lots <= 0:
+			pass
+		pass
 
 func _on_dice_roll_done(roll: int) -> void:
 	roll_total += roll
 	dice_rolled += 1
 	
+	# Doesn't count a roll until all dice are scored
 	if dice_rolled == dice_in_play:
-		rolls -= 1
+		rerolls -= 1
 		update_score(roll_total)
 		
 		# Reset for new roll
@@ -25,4 +38,9 @@ func _on_dice_roll_done(roll: int) -> void:
 
 func update_score(score_total: int):
 	score_label.text = str(score_total)
-	rolls_label.text = "Rolls: " + str(rolls)
+	lots_label.text = "Lots: " + str(lots)
+	reroll_label.text = "Rerolls: " + str(rerolls)
+	
+# func enter_shop():
+	# Buy random game piece
+	# Return to game with reverted lot and reroll count
