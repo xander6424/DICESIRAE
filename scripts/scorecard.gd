4fd7dac9b759ae76e7d7909826a57c46990169c9
xander6424 <xanderigross@gmail.com>
2026-06-18 13:54:44 -0400

    Initialize Scorecard
    The scorecard now correctly accesses the category information when starting the game.

--- scripts/scorecard.gd
+++ scripts/scorecard.gd
@@ -4,47 +4,43 @@ extends Control
 @onready var category_label_list = [%CategoryLabel1, %CategoryLabel2, %CategoryLabel3, %CategoryLabel4, %CategoryLabel5]
 @onready var category_button_list = [%CategoryButton1, %CategoryButton2, %CategoryButton3, %CategoryButton4, %CategoryButton5]
 
-var game_scorecard_info = ["Aces", "Twos", "Threes", "Two Pair", "Three of a Kind"]
-
 enum Categories {ACES, TWOS, THREES, FOURS, TWO_PAIR, THREE_OF_A_KIND}
+var starting_category_list = [Categories.ACES, Categories.TWOS, Categories.THREES, Categories.TWO_PAIR, Categories.THREE_OF_A_KIND]
 
 class CategoryInfo:
 	var name: String
 	var base_score: int
 	var id: Categories
+	var scored: bool = false
 	
 	func _init(name: String, base_score: int, id: Categories):
 		self.name = name
 		self.base_score = base_score
 		self.id = id
 
+# Initialize category name, base score, and id
 var aces = CategoryInfo.new("Aces", 5, Categories.ACES)
 var twos = CategoryInfo.new("Twos", 5, Categories.TWOS)
 var threes = CategoryInfo.new("Threes", 10, Categories.THREES)
 var fours = CategoryInfo.new("Fours", 10, Categories.FOURS)
 var two_pair = CategoryInfo.new("Two Pair", 20, Categories.TWO_PAIR)
 var three_of_a_kind = CategoryInfo.new("Three of a Kind", 30, Categories.THREE_OF_A_KIND)
-var category_list = [aces, twos, threes, fours, two_pair, three_of_a_kind]
+var category_info_list = [aces, twos, threes, fours, two_pair, three_of_a_kind]
 
 func _ready() -> void:
+	var scorecard_index: int = 0
+	for category in category_info_list:
+		if category.id in starting_category_list:
+			category_label_list[scorecard_index].text = category.name
+			category_button_list[scorecard_index].text = str(category.base_score) + " + 0"
+			category_button_list[scorecard_index].pressed.connect(category_button_pressed)
+			scorecard_index += 1
 	
-	
-	
-	# Put each category name on scorecard
-	var i = 0
-	for category in category_label_list:
-		category.text = game_scorecard_info[i]
-		i += 1
-	
-	# Initialize button press functions and scores
-	for category in category_button_list:
-		category.text = "0"
-		category.pressed.connect(category_button_pressed)
 	score_button.pressed.connect(score_button_pressed)
 
 
 func category_button_pressed() -> void:
-	pass
+	print("PRESSED")
 
 func score_button_pressed() -> void:
 	var category_selected: bool = false
