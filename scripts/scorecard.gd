fbe45dd4409e71727cdd84f67ec896c31a7c80f1
xander6424 <xanderigross@gmail.com>
2026-06-18 14:51:11 -0400

    Update Scorecard Score
    Scorecard correctly displays the rolled total for each (basic) category. The label and button is also assigned to each individual category.

--- scripts/scorecard.gd
+++ scripts/scorecard.gd
@@ -10,8 +10,12 @@ var starting_category_list = [Categories.ACES, Categories.TWOS, Categories.THREE
 class CategoryInfo:
 	var name: String
 	var base_score: int
+	var total: int = 0
 	var id: Categories
 	var scored: bool = false
+	
+	var label: Label = null
+	var button: Button = null
 	
 	func _init(name: String, base_score: int, id: Categories):
 		self.name = name
@@ -26,14 +30,20 @@ var fours = CategoryInfo.new("Fours", 10, Categories.FOURS)
 var two_pair = CategoryInfo.new("Two Pair", 20, Categories.TWO_PAIR)
 var three_of_a_kind = CategoryInfo.new("Three of a Kind", 30, Categories.THREE_OF_A_KIND)
 var category_info_list = [aces, twos, threes, fours, two_pair, three_of_a_kind]
+var active_category_info_list = []
 
 func _ready() -> void:
 	var scorecard_index: int = 0
 	for category in category_info_list:
 		if category.id in starting_category_list:
-			category_label_list[scorecard_index].text = category.name
+			category_label_list[scorecard_index].text = category.name + ":"
 			category_button_list[scorecard_index].text = str(category.base_score) + " + 0"
 			category_button_list[scorecard_index].pressed.connect(category_button_pressed)
+			
+			category.label = category_label_list[scorecard_index]
+			category.button = category_button_list[scorecard_index]
+			
+			active_category_info_list.append(category)
 			scorecard_index += 1
 	
 	score_button.pressed.connect(score_button_pressed)
@@ -58,34 +68,35 @@ func score_button_pressed() -> void:
 
 
 func _on_update_scorecard() -> void:
-	var aces_total = 0
-	var twos_total = 0
-	var threes_total = 0
-	
-	# Aces
+	var scorecard_dice_list = []
 	for dice in Global.rolling_dice_list:
-		if dice == 1:
-			aces_total += dice
+		scorecard_dice_list.append(dice)
 	for dice in Global.saved_dice_list:
-		if dice == 1:
-			aces_total += dice
+		scorecard_dice_list.append(dice)
 	
-	# Twos
-	for dice in Global.rolling_dice_list:
-		if dice == 2:
-			twos_total += dice
-	for dice in Global.saved_dice_list:
-		if dice == 2:
-			twos_total += dice
-	
-	# Threes
-	for dice in Global.rolling_dice_list:
-		if dice == 3:
-			threes_total += dice
-	for dice in Global.saved_dice_list:
-		if dice == 3:
-			threes_total += dice
-	
-	category_button_list[0].text = str(aces_total)
-	category_button_list[1].text = str(twos_total)
-	category_button_list[2].text = str(threes_total)
+	for category in active_category_info_list:
+		category.total = 0
+		
+		match category.id:
+			Categories.ACES:
+				for dice in scorecard_dice_list:
+					if dice == 1:
+						category.total += dice
+			Categories.TWOS:
+				for dice in scorecard_dice_list:
+					if dice == 2:
+						category.total += dice
+			Categories.THREES:
+				for dice in scorecard_dice_list:
+					if dice == 3:
+						category.total += dice
+			Categories.FOURS:
+				for dice in scorecard_dice_list:
+					if dice == 4:
+						category.total += dice
+			Categories.TWO_PAIR:
+				pass
+			Categories.THREE_OF_A_KIND:
+				pass
+		
+		category.button.text = str(category.base_score) + " + " + str(category.total)
