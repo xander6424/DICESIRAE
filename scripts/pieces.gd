4c776f446931584b2fa112299f1026aa0923c438
xander6424 <xanderigross@gmail.com>
2026-06-30 10:19:36 -0400

    Game Piece Resource
    Created a base resource script for game pieces.

---
+++ scripts/pieces.gd
@@ -0,0 +1,23 @@
+extends Resource
+
+class_name PieceInstance
+
+@export var piece_id: String # change to enum?
+@export var piece_name: String
+@export var description: String
+@export var texture: Texture2D
+
+@export var sell_value: int
+
+
+func piece_scored():
+	pass
+
+func dice_scored():
+	pass
+
+func round_started():
+	pass
+
+func round_ended():
+	pass
