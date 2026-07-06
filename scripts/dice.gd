975c7a5832bcdfb1d3a4625ab5a4d63963f649f9
xander6424 <xanderigross@gmail.com>
2026-07-06 19:34:04 -0400

    Dice Face Assets
    Moved the dice face assets into a single modifiable array.

--- scripts/dice.gd
+++ scripts/dice.gd
@@ -2,16 +2,19 @@ extends RigidBody2D
 
 class_name DiceNode
 
-signal _update_round_status()
+signal _update_round_status() # FIX and move this to dice manager!!!
 
-@onready var faces: Node2D = %Faces
+@onready var face_sprite: Sprite2D = %FaceSprite
 @onready var roll_button = get_parent().get_parent().get_node("RollButton") # THIS SUCKS
 @onready var save_button: TextureButton = %SaveButton
 
 @export var dice: DiceInfo
+@export var face_textures: Array[Texture2D] = []
+
 static var total_dice_rolled: int = 0
 static var dice_in_play: int = 5
 static var any_dice_rolling = false
+
 var number_rolled: int = 0
 var rolling: bool = false
 var dice_saved: bool = false
@@ -21,12 +24,12 @@ func _ready() -> void:
 	if dice == null:
 		dice = DiceInfo.new()
 	
-	for face in faces.get_children():
-		face.hide()
+	#for face in faces.get_children():
+		#face.hide()
 	
 	# Select a starting face at random
-	var starting_index: int = faces.get_children().pick_random().get_index()
-	faces.get_child(starting_index).show()
+	#var starting_index: int = faces.get_children().pick_random().get_index()
+	#faces.get_child(starting_index).show()
 	
 	roll_button.pressed.connect(roll_button_pressed)
 	save_button.pressed.connect(_save_button_pressed)
@@ -38,7 +41,6 @@ func setup(new_dice: DiceInfo):
 	
 	# faces display
 	# color change
-
 
 
 func roll_button_pressed():
@@ -54,20 +56,20 @@ func roll_dice():
 	rolling = true
 	
 	var duration: float = GameData.ROLL_DURATION
-	var current_index: int = faces.get_children().find(faces.get_children().filter(func(f): return f.visible)[0])
-	var face_count: int = faces.get_child_count()
+	#var current_index: int = faces.get_children().find(faces.get_children().filter(func(f): return f.visible)[0])
+	#var face_count: int = faces.get_child_count()
 	
 	# Roll the dice
 	while duration > 0:
-		var new_index: int = current_index
+		#var new_index: int = current_index
 		
 		# Prevent duplicate faces from being shown in a row
-		while current_index == new_index:
-			new_index = randi() % face_count
+		#while current_index == new_index:
+			#new_index = randi() % face_count
 		
-		faces.get_child(current_index).hide()
-		faces.get_child(new_index).show()
-		current_index = new_index
+		#faces.get_child(current_index).hide()
+		#faces.get_child(new_index).show()
+		#current_index = new_index
 		
 		await get_tree().create_timer(0.1).timeout
 		duration -= 0.1
@@ -75,7 +77,7 @@ func roll_dice():
 	# Finish rolling dice and add it to the list
 	rolling = false
 	total_dice_rolled += 1
-	number_rolled = int(faces.get_child(current_index).name)
+	#number_rolled = int(faces.get_child(current_index).name)
 	GameData.rolling_dice_list.append(number_rolled)
 	
 	# Doesn't count a roll until all dice are scored
