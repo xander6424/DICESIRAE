937f1ce7c6a654b8b200e65ba6a813c969023105
xander6424 <xanderigross@gmail.com>
2026-07-06 14:22:41 -0400

    Dice Class Outline
    Modified the dice class and created an outline of features.

--- scripts/classes/dice/dice-class.gd
+++ scripts/classes/dice/dice-class.gd
@@ -2,11 +2,23 @@ extends Resource
 
 class_name DiceInfo
 
-@export var size: DiceData.Size
-@export var type: DiceData.DiceType
-var faces: Array[int] = [1, 2, 3, 4, 5, 6]
-# a variable type that can store both an int and face type?
-# along with a function method?
+@export var dice_size: DiceData.Size = DiceData.Size.D6
+@export var dice_type: DiceData.DiceType = DiceData.DiceType.NORMAL
+var faces: Array[DiceFace] = []
+
+# current face index/value?
+
+
+func _init(_dice_size: DiceData.Size = DiceData.Size.D6, _dice_type: DiceData.DiceType = DiceData.DiceType.NORMAL) -> void:
+	dice_size = _dice_size
+	dice_type = _dice_type
+	
+	if faces.is_empty():
+		generate_default_faces()
+
+func generate_default_faces() -> void:
+	for i in range(1, dice_size + 1):
+		faces.append(DiceFace.new(i, DiceData.FaceType.NORMAL))
 
 
 func roll() -> int:
@@ -14,8 +26,12 @@ func roll() -> int:
 	# an array of size "DiceData.Size"??
 	return 0
 
-func save() -> void:
-	pass
+# Get current face?
+
+# Upgrade face?
+
+# Upgrade size?
+
 
 func score_dice() -> int:
 	return 0
