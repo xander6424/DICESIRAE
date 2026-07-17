59bc1678197420adc391856b69df41b7a231d8b2
xander6424 <xanderigross@gmail.com>
2026-07-17 15:31:17 -0400

    Piece Hint Display
    Further adjusted the piece display and added a hint component when hovering over the piece.

--- scripts/piece-display.gd
+++ scripts/piece-display.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 class_name PieceDisplay
 
@@ -6,9 +6,12 @@ class_name PieceDisplay
 
 @export var piece: PieceInfo
 
+@onready var piece_area: Area2D = %PieceArea
 @onready var piece_sprite: Sprite2D = %PieceSprite
 @onready var piece_collision_shape: CollisionShape2D = %PieceCollisionShape
-
+@onready var hint_display: Control = %HintDisplay
+@onready var piece_name_label: Label = %PieceName
+@onready var piece_description_label: Label = %PieceDescription
 
 
 func _ready() -> void:
@@ -16,7 +19,17 @@ func _ready() -> void:
 		piece = PieceInfo.new()
 	
 	piece_sprite.texture = piece.texture
+	hint_display.visible = false
 
 func setup(new_piece: PieceInfo) -> void:
 	piece = new_piece
 	piece_sprite.texture = piece.texture
+	piece_name_label.text = piece.piece_name
+	piece_description_label.text = piece.piece_description
+
+
+func _on_mouse_entered() -> void:
+	hint_display.visible = true
+
+func _on_mouse_exited() -> void:
+	hint_display.visible = false
